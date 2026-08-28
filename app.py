53872f0f277b9311336777ae94cc40189ea3bd20
rkgcodes <filmcraft09@gmail.com>
2026-08-28 19:26:45 +0530

    few changes here and there

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
 
 
-
 def main():    
-    list1=[30,20,3000,5000]
+    list1=[30,20,3000,5000, None, None]
     list2=[list1[0], list1[1]]
     x=dashboard()
-    while(x==2): 
-        if(x==2):       
+    y=1
+
+    while (y==1):
+        if (x==2):              
             list2=available_bedcount(list1)
-            list1.extend(list2)        
+            list1[4]=list2[0]
+            list1[5]=list2[1]        
             x=dashboard(*list1)
-            continue
+                       
 
-        
-
-    while(x==1): 
-        if(x==1):       
+       
+        elif (x==1):               
             list1=modify_admin_info() 
             list2=[list1[0], list1[1]]      
-            list1.extend(list2)
+            list1[4]=list2[0]
+            list1[5]=list2[1] 
             x = dashboard(*list1)
-            continue
+        
+        else:
+            y=2
+            break
 
-    
-
-
-    
+       
 
 def dashboard(num1=30, num2=20, rate1=3000, rate2=5000, avail1=30, avail2=20):
     
@@ -39,14 +40,16 @@ def dashboard(num1=30, num2=20, rate1=3000, rate2=5000, avail1=30, avail2=20):
     print("Total no. of ICU bed: ", num1 + num2)
     print()
     print("-"*100)
+    print("Total normal ICU bed without ventilator: ", num1)
+    print("Currently occupied normal ICU bed without ventilator: ", num1-avail1)
     print("Available ICU bed without ventilator: ", avail1)
-    print("Rate: ₹",rate1)
-    print("Currently occupied: ", num1-avail1)
+    print("Rate: ₹",rate1)    
     print()
     print("-"*100)
+    print("Total normal ICU bed with ventilator: ", num2)
+    print("Currently occupied ICU bed with ventilator: ", num2-avail2)
     print("Available ICU bed with ventilator: ", avail2)
-    print("Rate: ₹", rate2)
-    print("Currently occupied: ", num2-avail2)
+    print("Rate: ₹", rate2)    
     print()
     print("-"*100)    
     return int(input("Press 1 to go to Admin login || Press 2 to modify occupancy: "))
@@ -62,7 +65,7 @@ def modify_admin_info():
     bednum2=int(input("Enter the total no. of ICU bed with ventilor: "))
     bedrate1=int(input("Enter the rate of ICU bed without ventilor in ₹: "))
     bedrate2=int(input("Enter the rate of ICU bed with ventilor in ₹: "))
-    return [bednum1, bednum2, bedrate1, bedrate2]
+    return [bednum1, bednum2, bedrate1, bedrate2, None, None]
 
 
 
