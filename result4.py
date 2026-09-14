6c2ef7745bedafbae1d64f8bd0cb46c62a8c813e
rkgcodes <filmcraft09@gmail.com>
2026-09-15 00:09:14 +0530

    DB & UI integration

--- result4.py
+++ result4.py
@@ -1,12 +1,66 @@
+import customtkinter as ctk
+ctk.set_appearance_mode("dark")
+result4 = ctk.CTk()
+result4.title("Excercise")
+result4.geometry("600x400")
+
+
+
+
 import sqlite3
-
 connection = sqlite3.connect("hospital.db")
-
 cursor = connection.cursor()
 
 
 
-# values before update
+
+
+def get_occupied_beds():
+    occupied=num.get()
+    update_occupied(1, "ventilator", occupied)
+
+
+
+
+#result_label.configure(text="Hello " + entered_name)
+
+
+
+header = ctk.CTkFrame(result4)
+header.pack()
+
+title = ctk.CTkLabel(
+    header,
+    text="UI & DB Integartion"
+)
+
+
+title.pack(pady=20)
+
+
+label = ctk.CTkLabel(result4, text="Enter occupied beds")
+label.pack()
+
+num = ctk.CTkEntry(result4)
+
+num.pack(pady=10)
+
+
+button = ctk.CTkButton(
+    result4,
+    text="CLICK ME",
+    command=get_occupied_beds
+    
+)
+button.pack(pady=10)
+
+result_label = ctk.CTkLabel(result4, text="")
+result_label.pack()
+
+
+
+
+
 print("values before update")
 
 cursor.execute(" SELECT * FROM icu_beds")
@@ -45,12 +99,10 @@ def update_occupied(level, bed_type, occupied):
        connection.commit()
                
     except ValueError:
-        print("Enter occupied beds number only")
+        print("Cannot be greater than total beds")
 
         
-        
-
-update_occupied(1, "ventilator", "abc")
+    
 
 #values after update
 print("values after update")
@@ -64,3 +116,9 @@ for row in rows:
 
 
 connection.close()
+
+
+
+
+
+result4.mainloop()
